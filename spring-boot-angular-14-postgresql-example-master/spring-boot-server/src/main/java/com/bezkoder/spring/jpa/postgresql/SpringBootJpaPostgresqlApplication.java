d94ad99e13e16c5a70a5f5b839a0a15d4154763c
Paulguzhnay <paul-guzhnay@hotmail.com>
2022-12-11 10:35:08 -0500

    Commit

---
+++ spring-boot-angular-14-postgresql-example-master/spring-boot-server/src/main/java/com/bezkoder/spring/jpa/postgresql/SpringBootJpaPostgresqlApplication.java
@@ -0,0 +1,28 @@
+package com.bezkoder.spring.jpa.postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class SpringBootJpaPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaPostgresqlApplication.class, args);
+		
+		
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
+
+}
